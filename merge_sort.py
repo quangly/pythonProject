718dae0b5305575a9fc5e9341b95040811ea4eb4
Quang Ly <quangdly@gmail.com>
2021-01-24 18:35:37 -0500

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,68 @@
+# Merge Sort is a Divide and Conquer algorithm. It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one. See the following C implementation for details.
+#
+# MergeSort(arr[], l,  r)
+# If r > l
+#      1. Find the middle point to divide the array into two halves:
+#              middle m = (l+r)/2
+#      2. Call mergeSort for first half:
+#              Call mergeSort(arr, l, m)
+#      3. Call mergeSort for second half:
+#              Call mergeSort(arr, m+1, r)
+#      4. Merge the two halves sorted in step 2 and 3:
+#              Call merge(arr, l, m, r)
+# refer: https://www.geeksforgeeks.org/merge-sort/
+
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        # Finding the mid of the array
+        mid = len(arr)//2
+
+        # Dividing the array elements
+        L = arr[:mid]
+        R = arr[mid:]
+
+        # Sorting the first half
+        mergeSort(L)
+
+        #Sorting the second half
+        mergeSort(R)
+
+        i = j = k = 0
+
+        #Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end = " ")
+    print()
+
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
+
