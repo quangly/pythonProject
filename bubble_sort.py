3c72056023ffee73bfe5c152433d8176e05ea354
Quang Ly <quangdly@gmail.com>
2021-01-26 22:01:53 -0500

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(list_a):
+    indexing_length = len(list_a) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+
+        for i in range(0, indexing_length):
+            if list_a[i] > list_a[i + 1]:
+                sorted = False
+                list_a[i], list_a[i+1] = list_a[i + 1], list_a[i]
+    return list_a
+
+
+print(bubble_sort([2,1,4,5,0,9,7]))
+
