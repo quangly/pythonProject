8be2a63dfbabfbbba2900e800fb9b41a789e153a
Quang Ly <quangdly@gmail.com>
2021-01-27 09:12:26 -0500

    check unbalanced parentheses - cleanup

--- check_balanced_parentheses.py
+++ check_balanced_parentheses.py
@@ -5,15 +5,14 @@ close_list = ["]", "}", ")"]
 
 
 # Function to check parentheses
-def check(myStr):
+def check(my_str):
     stack = []
-    for i in myStr:
+    for i in my_str:
         if i in open_list:
             stack.append(i)
         elif i in close_list:
             pos = close_list.index(i)
-            if ((len(stack) > 0) and
-                    (open_list[pos] == stack[len(stack) - 1])):
+            if (len(stack) > 0) and (open_list[pos] == stack[len(stack) - 1]):
                 stack.pop()
             else:
                 return "Unbalanced"
