ad6077320b8a859f15ed905fefd9e5bfef48a015
Quang Ly <quangdly@gmail.com>
2021-01-26 20:40:06 -0500

    quick sort simplified

--- quick_sort.py
+++ quick_sort.py
@@ -6,45 +6,24 @@
 # to left of pivot and all greater elements to right
 # of pivot
 
-def partition(arr, low, high):
-    i = (low - 1)       # index of smaller element
-    pivot = arr[high]   #pivot
+def quick_sort(sequence):
+    items_greater = []
+    items_lower = []
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
 
-    for j in range(low, high):
+        for item in sequence:
+            if item > pivot:
+                items_greater.append(item)
+            else:
+                items_lower.append(item)
 
-        # If current element is smaller than the pivot
-        if arr[j] < pivot:
+        return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
-            # increment index of smaller element
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
 
-    arr[i+1], arr[high] = arr[high], arr[i+1]
-    return (i+1)
-
-# The main function that implements QuickSort
-# arr[] --> Array to be sorted,
-# low  --> Starting index,
-
-# high  --> Ending index
-def quickSort(arr, low, high):
-    if low < high:
-
-        # pi is partitioning index, arr[p] is now
-        # at right place
-        pi = partition(arr, low, high)
-
-        # Separately sort elements before
-        # partition and after partition
-
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi+1, high)
-
-# Driver code to test above
 arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-print("Original array is:")
-print(arr)
-quickSort(arr,0,n-1)
-print("Sorted array is:")
-print(arr)
+print("Array: " + str(arr))
+print("Quick Sorted Array: " + str(quick_sort(arr)))
